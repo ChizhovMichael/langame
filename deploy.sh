2522046f4cc8fcdace174ce0ad9a073150177cb0
Chizhov Michael <chizhov.michael@gmail.com>
2023-03-06 19:37:22 +0400

    setting docker

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,13 @@
 docker-compose up -d
-sleep 5
-docker-compose exec php php artisan migrate
-sleep 5
-docker-compose exec php php artisan post:import
-sleep 5
+while :
+do
+    if [[ -f ${PWD}/vendor/autoload.php ]]
+    then
+        sleep 10
+        docker-compose exec php php artisan migrate
+        docker-compose exec php php artisan post:import
+        break
+    fi
+    sleep 5
+done
 echo "Start website: localhost:80"
